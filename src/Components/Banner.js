5a22dc03f428cc601e72c9ac5fa4164748be8b95
Faiz4211 <faizk2631@gmail.com>
2023-02-09 14:58:48 +0500

    updated

---
+++ src/Components/Banner.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../App.css';
+
+export default function Banner() {
+    return (
+        <>
+            <div id="banner" className='py-5'>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-8 col-lg-6">
+                            <div className="banner-title">
+                                <h1 className='title'>Some Heading Goes here</h1>
+                                <p className='lead'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident ipsum aspernatur dolorum debitis, doloribus molestias perspiciatis fugiat atque. Cupiditate, culpa!</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
